bcee873d77d52d55167b1063595e2579ddb9825c
sharika1232 <enagasharika@gmail.com>
2026-01-07 17:03:38 +0530

    Add files via upload
    Datatypes, variables and IO functions

---
+++ Day1.py
@@ -0,0 +1,4 @@
+
+print("I Am Sharika")
+print("Day 1 of python at Codegnan")
+print("I Am performing some new operation") 
---
+++ Day22.py
@@ -0,0 +1,20 @@
+decimal_val=10.25
+name="Sharika"
+age=21
+list=[1,2,"2",4,6,3.7]
+tuple=(3,56,894,42,[56,99,67],66)
+set={1,3,4,67,98,24,90}
+dict={"a":56,67:"sarika",678:789}
+cards={"a","hello","sarika","hi",635,8,67}
+student_marks={"math":90,'tel':75,"eng":80,"sci":56}
+weight=588.5
+
+
+print("my name is:", name)
+print("my age is:", age)
+print("my weight is:", weight)
+print(tuple,tuple)
+print(set,set)
+print(dict)
+print(cards)
+print("list of student_marks:", student_marks)
